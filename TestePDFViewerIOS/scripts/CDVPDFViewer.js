1b7082c067b086342d86830963f71522e29b997a
Antonio Carlos Souza Zechin <acs.zechin@knowlodge.com.br>
2014-04-08 16:12:36 -0500

    Changes made by Antonio Carlos Souza Zechin (acs.zechin@knowlodge.com.br).

--- TestePDFViewerIOS/scripts/CDVPDFViewer.js
+++ TestePDFViewerIOS/scripts/CDVPDFViewer.js
@@ -12,7 +12,7 @@
  var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
  
  function CDVPDFViewer() {
- // Does nothing
+ 	// Does nothing
  }
  
   
@@ -20,7 +20,7 @@
  // called from native
  CDVPDFViewer._onClose = function()
  {
- window.plugins.CDVPDFViewer.onClose();
+ 	window.plugins.CDVPDFViewer.onClose();
  };
  
  
@@ -30,13 +30,13 @@
  // Show a webpage, will result in a callback to onLocationChange
  CDVPDFViewer.prototype.showPDF = function(loc)
  {
- cordovaRef.exec("CDVPDFViewer.showPDF", loc);
+ 	cordovaRef.exec("CDVPDFViewer.showPDF", loc);
  };
  
  // close the browser, will NOT result in close callback
  CDVPDFViewer.prototype.close = function()
  {
- cordovaRef.exec("CDVPDFViewer.close");
+ 	cordovaRef.exec("CDVPDFViewer.close");
  };
  
  // Not Implemented
@@ -50,14 +50,14 @@
  // it will be returned, and also available globally from window.plugins.PDFViewer
  CDVPDFViewer.install = function()
  {
- if(!window.plugins) {
-
- window.plugins = {};
- }
- if ( ! window.plugins.CDVPDFViewer ) {
-
- window.plugins.CDVPDFViewer = new CDVPDFViewer();
- }
+    if(!window.plugins) {
+    
+     	window.plugins = {};
+ 	}
+    if ( ! window.plugins.CDVPDFViewer ) {
+    
+       window.plugins.CDVPDFViewer = new CDVPDFViewer();
+    }
  
  };
  
